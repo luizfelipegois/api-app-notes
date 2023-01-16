b97972576259ab1939acc9a99932f5ec19c77938
eulipegois <luizz3.14@hotmail.com>
2023-01-16 18:19:03 -0500

    feat: Route management for user authentication

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const databaseConnection = require('./database');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/auth', userRoutes);
 
 app.listen(process.env.PORT || 3001, databaseConnection());
